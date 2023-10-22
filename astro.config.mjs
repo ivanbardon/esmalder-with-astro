a30a1c590dff3573ccd2c9dddea063a482c509f7
Ivan Bardon <ivan@MBP-de-ivan-2.home>
2023-10-22 14:37:09 +0200

    adding deply to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ivanbardon.github.io',
+  base: '/esmalder-with-astro',
+})
